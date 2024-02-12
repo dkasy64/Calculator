c96a8058963ef9b98b4fc4ffba8e12a016be480d
dkasy64 <dawitkasy64@gmail.com>
2024-02-12 16:51:10 -0500

    Multiplication and division operations added

--- Calculator.java
+++ Calculator.java
@@ -8,13 +8,17 @@ public class Calculator {
         int x = scan.nextInt();
         int y = scan.nextInt();
         
-        System.out.println("Type 1 to perform addition\nType 2 to perform subtraction");
+        System.out.println("Type 1 to perform addition\nType 2 to perform subtraction\nType 3 to perform multiplication\nType 4 to perform division");
         int operation = scan.nextInt();
         
         if(operation == 1){
             System.out.println(x+y);
         } else if(operation == 2) {
             System.out.println(x-y);
+        } else if(operation == 3) {
+            System.out.println(x*y);
+        } else if(operation == 4) {
+            System.out.println(x/y);
         }
 
     }
