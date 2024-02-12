d41bcd13c05e05cbf7b4e726d7acdf7a25b067f6
dkasy64 <dawitkasy64@gmail.com>
2024-02-12 17:07:06 -0500

    Error: divide by zero added

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,9 @@ public class Calculator {
         System.out.println("Type 1 to perform addition\nType 2 to perform subtraction\nType 3 to perform multiplication\nType 4 to perform division");
         double operation = scan.nextInt();
         
-        if(operation == 1){
+        if(y == 0 && operation == 4) {
+            System.out.println("Error: Divide by Zero");
+        } else if(operation == 1){
             System.out.println(x+y);
         } else if(operation == 2) {
             System.out.println(x-y);
