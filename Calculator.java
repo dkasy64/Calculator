142fa6c115706a96a7b5754ed1cd80bcd0ef8e02
dkasy64 <dawitkasy64@gmail.com>
2024-02-12 16:09:15 -0500

    Put code in Calculator file

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+
+    public static void main(String[] args){
+        System.out.println("This is a calculator file");
+        System.out.println("This is another change");
+    }
+}
