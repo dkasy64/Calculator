63037c98e291f18f784127e8fd71045c493d987c
dkasy64 <dawitkasy64@gmail.com>
2024-02-12 16:40:42 -0500

    Ask to perform subtraction or addition and Subtraction operation added

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,15 @@ public class Calculator {
         System.out.println("Enter two whole numbers");
         int x = scan.nextInt();
         int y = scan.nextInt();
+        
+        System.out.println("Type 1 to perform addition\nType 2 to perform subtraction");
+        int operation = scan.nextInt();
+        
+        if(operation == 1){
+            System.out.println(x+y);
+        } else if(operation == 2) {
+            System.out.println(x-y);
+        }
 
-        System.out.println(y+x);
     }
 }
